1f932329e989c01e860c416068fc4ff04736f56c
Shani Banerjee <shanibanerjee@gmail.com>
2019-12-26 15:34:35 -0500

    fixed styling and typos

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,24 +6,23 @@ let Song = EmberObject.extend({
     title: '',
     band: '',
     rating: 0
-
 });
 
 export default Route.extend({
     model() {
-        let blackDog = EmberObject.create({
+        let blackDog = Song.create({
             title: 'Black Dog',
             band: 'Led Zepplin',
             rating: 3
         });
 
-        let yellowLedbetter = EmberObject.create({
+        let yellowLedbetter = Song.create({
             title: 'Yellow Ledbetter',
             band: 'Pearl Jam',
             rating: 4
         });
 
-        let pretender = EmberObject.create({
+        let pretender = Song.create({
             title: 'The Pretender',
             band: 'Foo Fighters',
             rating: 2
