1f2babf99df0313ff6e15125b7f268dd79c2176c
Shani Banerjee <shanibanerjee@gmail.com>
2020-01-08 15:32:05 -0500

    Revert "Update band route in bands"
    This reverts commit f2ad7347a4a3a33fe9735a3dce773ea3b6376bd8.

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(params) {
-      let bands = this.modelFor('bands');
-      return bands.findBy('slug', params.slug);
-    }
 });
