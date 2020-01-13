beafd181923c01fd8c563a61ce4fd59c91c6e3a6
Shani Banerjee <shanibanerjee@gmail.com>
2020-01-13 14:52:14 -0500

    Removed models from routes, added save method to bands controller

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,9 +1,10 @@
 import Controller from '@ember/controller';
+import Band from 'rarwe/models/band';
 
 export default Controller.extend({
     isAddingBand: false,
     newBandName: '',
-    
+
     actions: {
         addBand() {
             this.set('isAddingBand', true);
@@ -11,9 +12,10 @@ export default Controller.extend({
 
         cancelAddBand() {
             this.set('isAddingBand', false);
-        }
+        },
         saveBand() {
-            //Create New Band
+            let newBand = Band.create({ name: this.newBandName });
+            this.model.pushObject(newBand);
         }
     }
 });
