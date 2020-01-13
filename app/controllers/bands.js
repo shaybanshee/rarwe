7e1a1d0f3ab7dbead56cc072f20ef98183d1d614
Shani Banerjee <shanibanerjee@gmail.com>
2020-01-13 13:03:29 -0500

    Cancel method added to turn isAddingBand to false

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -2,9 +2,18 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
     isAddingBand: false,
+    newBandName: '',
+    
     actions: {
         addBand() {
             this.set('isAddingBand', true);
         },
+
+        cancelAddBand() {
+            this.set('isAddingBand', false);
+        }
+        saveBand() {
+            //Create New Band
+        }
     }
 });
