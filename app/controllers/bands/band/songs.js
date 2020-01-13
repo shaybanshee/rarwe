717c0fc89249f0d0b6cb63dbcf2c9ac970eabb63
Shani Banerjee <shanibanerjee@gmail.com>
2020-01-13 17:53:39 -0500

    Songs Controller added

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,29 @@
+import Controller from '@ember/controller';
+import Song from 'rarwe/models/song';
+import { empty } from '@ember/object/computed';
+
+
+export default Controller.extend({
+    isAddingSong: false,
+    newSongName: '',
+
+    isAddButtonDisabled: empty('newSongTitle'),
+
+    actions: {
+    
+        addSong()   {
+            this.set('isAddingSong', true);
+        },
+
+        cancelAddSong() {
+            this.set('isAddingSong', false);
+        },
+
+        saveSong()  {
+            event.preventDefault();
+            let newSong = Song.create({ title: this.newSongTitle });
+            this.model.songs.pushObject(newSong);
+            this.set('newSongTitle', '');
+        },
+    }
+});
