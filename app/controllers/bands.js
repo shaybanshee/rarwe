033ec0da8ff7dc1ff576378fbfc3424f1b77547f
Shani Banerjee <shanibanerjee@gmail.com>
2020-01-14 12:17:37 -0500

    Song adding ability added

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -16,12 +16,16 @@ export default Controller.extend({
         cancelAddBand() {
             this.set('isAddingBand', false);
         },
-        
+
         saveBand(event) {
             event.preventDefault();
             let newBand = Band.create({ name: this.newBandName });
             this.model.pushObject(newBand);
-            this.set('newBandName', '');
+            this.setProperties({
+                newBandName: '',
+                isAddingBand: false
+            });
+            this.transitionToRoute('bands.band.songs', newBand);
         }
     }
 });
